5d244c5ae07fb921c0ef81edb3132a599f6a40f0
AshwinGour28 <ashwing2884@gmail.com>
2024-09-14 18:14:19 +0530

    added pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 export default function App() {
   return (
-    <div className='text-3xl text-red-500'>
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
