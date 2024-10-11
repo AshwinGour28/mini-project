d637e518398ad1ad50a7bc280beceddf1b62c28b
Utkarsh-dot-hi <121947436+Utkarsh-dot-hi@users.noreply.github.com>
2024-10-11 19:52:58 +0530

    Updated Footer component for better aesthetics

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,19 +1,40 @@
-import { Footer } from 'flowbite-react'
-import {BsFacebook, BsInstagram, BsTwitter, BsGithub} from 'react-icons/bs'
-import React from 'react'
+import { Footer } from 'flowbite-react';
+import { BsFacebook, BsInstagram, BsTwitter, BsGithub } from 'react-icons/bs';
+import React from 'react';
 
 export default function FooterCom() {
   return (
-    <Footer container className='border-t-8 border-teal-500'>
-        <div className='flex gap-6 sm:mt-0 mt-4 sm:justify-center'>
-            <Footer.Icon href='#' icon={BsFacebook} />
-            <Footer.Icon href='#' icon={BsInstagram} />
-            <Footer.Icon href='#' icon={BsTwitter} />
-            <Footer.Icon href='#' icon={BsGithub} />
-        </div>
-        <div>
-            <Footer.Copyright href='#' by='NameOfSite' year={new Date().getFullYear()}/>
-        </div>
+    <Footer className="bg-gray-800 text-white border-t-8 border-teal-500 p-4">
+      <div className='flex gap-8 justify-center mt-4'>
+        <Footer.Icon 
+          href='#' 
+          icon={BsFacebook} 
+          className="text-2xl hover:text-teal-300 transition duration-300" 
+        />
+        <Footer.Icon 
+          href='#' 
+          icon={BsInstagram} 
+          className="text-2xl hover:text-teal-300 transition duration-300" 
+        />
+        <Footer.Icon 
+          href='#' 
+          icon={BsTwitter} 
+          className="text-2xl hover:text-teal-300 transition duration-300" 
+        />
+        <Footer.Icon 
+          href='#' 
+          icon={BsGithub} 
+          className="text-2xl hover:text-teal-300 transition duration-300" 
+        />
+      </div>
+      <div className='text-center mt-4'>
+        <Footer.Copyright 
+          href='#' 
+          by='Jet Set Go' 
+          year={new Date().getFullYear()} 
+          className='font-semibold'
+        />
+      </div>
     </Footer>
-  )
+  );
 }
