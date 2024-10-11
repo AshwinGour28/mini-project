3268e99b5ea60eef5919202ad39eba0832c65c49
Utkarsh-dot-hi <121947436+Utkarsh-dot-hi@users.noreply.github.com>
2024-10-11 19:48:22 +0530

    Updated Header.jsx to add hover effects and included logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,77 +1,74 @@
-import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react'
-import { Link } from 'react-router-dom'
-import { AiFillSwitcher, AiOutlineSearch } from 'react-icons/ai';
-import React from 'react'
+import React from 'react';
+import { Avatar, Button, Dropdown, Navbar } from 'flowbite-react';
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { signoutSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import logo from '../images/logoo.avif'; // Correct path to the logo
 
 export default function Header() {
-    const {currentUser} = useSelector(state => state.user);
+    const { currentUser } = useSelector((state) => state.user);
     const dispatch = useDispatch();
 
     const handleSignout = async () => {
         try {
-            const res = await fetch('http://localhost:3000/api/user/signout',{
+            const res = await fetch('http://localhost:3000/api/user/signout', {
                 method: 'POST',
             });
             const data = await res.json();
-            if(!res.ok){
+            if (!res.ok) {
                 console.log(data.message);
-            }
-            else{
+            } else {
                 dispatch(signoutSuccess());
             }
         } catch (error) {
-            console.log(error.message)
+            console.log(error.message);
         }
-    }
-  return (
-    <Navbar className='border-b-2'>
-        <Link to="/" className='self-center whitespace-nowrap text-xl font-semibold'>
-            <span>Name of site</span>
-            <span className='px-2 py-1 bg-red-500 rounded text-white'>Logo</span>
-        </Link>
-        <Navbar.Collapse>
-            <Navbar.Link>
-                <Link to='/'>Home</Link>
-            </Navbar.Link>
-            <Navbar.Link>
-                <Link to='/#'>Search Flights</Link>
-            </Navbar.Link>
-            <Navbar.Link>
-                <Link to='/about'>About</Link>
-            </Navbar.Link>
-        </Navbar.Collapse>
-        <div className='flex gap-2'>
-            <Button gradientDuoTone='redToYellow' className='mr-5'>Change Language</Button>
-            {currentUser ? (
-                <Dropdown arrowIcon={false} inline
-                label={
-                    <Avatar
-                        alt='user'
-                        img={currentUser.profilePicture}
-                        rounded
-                    />
-                }>
-                    <Dropdown.Header>
-                        <span className='block text-sm'>@{currentUser.u_name}</span>
-                        <span className='block text-sm font-medium truncate'>@{currentUser.email}</span>
-                    </Dropdown.Header>
-                    <Link to={'/dashboard?tab=profile'}>
-                    <Dropdown.Item>Profile</Dropdown.Item>
+    };
+
+    return (
+        <Navbar className="border-b-2 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 p-4">
+            <Link to="/" className="flex items-center">
+                <span className="text-3xl font-bold text-white font-poppins">Jet Set Go</span>
+                <img src={logo} alt="Logo" className="w-16 h-16 rounded-full inline-block ml-3" />
+            </Link>
+            <Navbar.Collapse>
+                <Navbar.Link>
+                    <Link to="/" className="text-2xl text-white hover:bg-white hover:bg-opacity-20 rounded-lg px-3 py-1 transition duration-300">Home</Link>
+                </Navbar.Link>
+                <Navbar.Link>
+                    <Link to="/#" className="text-2xl text-white hover:bg-white hover:bg-opacity-20 rounded-lg px-3 py-1 transition duration-300">Search Flights</Link>
+                </Navbar.Link>
+                <Navbar.Link>
+                    <Link to="/about" className="text-2xl text-white hover:bg-white hover:bg-opacity-20 rounded-lg px-3 py-1 transition duration-300">About</Link>
+                </Navbar.Link>
+            </Navbar.Collapse>
+            <div className="flex gap-2">
+                <Button gradientDuoTone="redToYellow" className="mr-5">Change Language</Button>
+                {currentUser ? (
+                    <Dropdown arrowIcon={false} inline label={
+                        <Avatar
+                            alt="user"
+                            img={currentUser.profilePicture}
+                            rounded
+                        />
+                    }>
+                        <Dropdown.Header>
+                            <span className="block text-sm">@{currentUser.u_name}</span>
+                            <span className="block text-sm font-medium truncate">@{currentUser.email}</span>
+                        </Dropdown.Header>
+                        <Link to={'/dashboard?tab=profile'}>
+                            <Dropdown.Item>Profile</Dropdown.Item>
+                        </Link>
+                        <Dropdown.Divider />
+                        <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
+                    </Dropdown>
+                ) : (
+                    <Link to="/login">
+                        <Button gradientDuoTone="purpleToBlue">Login/Register</Button>
                     </Link>
-                    <Dropdown.Divider />
-                    <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
-                </Dropdown>
-            ):
-            (
-                <Link to="/login">
-                    <Button gradientDuoTone='purpleToBlue'>Login/Register</Button>
-                </Link>
-            )}
-        </div>
-        
-    </Navbar>
-  )
+                )}
+            </div>
+        </Navbar>
+    );
 }
