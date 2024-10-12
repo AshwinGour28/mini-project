8c49112e1da45cc5cfc2c537074fe3a56c777979
AshwinGour28 <ashwing2884@gmail.com>
2024-10-12 13:38:57 +0530

    add delete user funtionality

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -66,3 +66,24 @@ export const updateUser = async (req, res, next) => {
     
     
 }
+
+
+export const deleteUser = async (req, res, next) => {
+    if (!req.user.isAdmin && req.user.id != req.params.userId) {
+        return next(errorHandler(400, "You are not allowed to delete this user"));
+    }
+
+    try {
+        const deletedUser = await User.destroy({
+            where: { reg_id: req.params.userId } 
+        });
+
+        if (deletedUser) {
+            res.status(200).json('User has been deleted');
+        } else {
+            res.status(404).json('User not found');
+        }
+    } catch (error) {
+        next(error);
+    }
+};
